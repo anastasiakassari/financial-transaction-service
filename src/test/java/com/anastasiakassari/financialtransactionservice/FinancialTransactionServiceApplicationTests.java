52dd6b48069dd08a39c374948d1da861057023b5
Anastasia Kassari <anastasia.kassari@axiomatics.com>
2023-07-03 18:11:35 +0300

    First project upload.

---
+++ src/test/java/com/anastasiakassari/financialtransactionservice/FinancialTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anastasiakassari.financialtransactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
